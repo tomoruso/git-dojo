d6a0718ff3bd02d0a4e3bbdd5174e37b9f0337b0
MattHowdy <mate.hodi9@gmail.com>
2018-03-27 10:01:16 +0200

    joke function has been defined

--- gittie.py
+++ gittie.py
@@ -1,1 +1,8 @@
 print("Hello! I'm Gittie")
+
+my_joke = 'I gave up jogging for health reasons. My thighs kept rubbing together and setting my pantyhose on fire.'
+
+def joke(message):
+    print(message)
+
+joke(my_joke)
