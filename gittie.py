dc864dc5ced5c129f4a610825c671cab4ae2b233
MattHowdy <mate.hodi9@gmail.com>
2018-03-27 10:10:52 +0200

    introduce function is added

--- gittie.py
+++ gittie.py
@@ -1,15 +1,19 @@
-print("Hello! I'm Gittie")
+my_message = "Hello! I'm Gittie"
 
-<<<<<<< HEAD
+def introduce(messages):
+    print(messages)
+
+introduce(my_message)
+
+
 def add_number():
     print (5,8)
 
 add_number()
-=======
 my_joke = 'I gave up jogging for health reasons. My thighs kept rubbing together and setting my pantyhose on fire.'
+
 
 def joke(message):
     print(message)
 
 joke(my_joke)
->>>>>>> d6a0718ff3bd02d0a4e3bbdd5174e37b9f0337b0
